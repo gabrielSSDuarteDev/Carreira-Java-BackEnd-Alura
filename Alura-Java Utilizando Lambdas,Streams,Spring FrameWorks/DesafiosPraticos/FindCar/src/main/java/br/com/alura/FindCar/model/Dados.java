e741f21d145f58c71f0f55626d560cb2c45ed738
Gabriel Soares De Souza Duarte <gabrielssduarte.dev@gmail.com>
2026-07-13 14:08:21 -0300

    Add FindCar

--- Alura-Java Utilizando Lambdas,Streams,Spring FrameWorks/DesafiosPraticos/FindCar/src/main/java/br/com/alura/FindCar/model/Dados.java
+++
@@ -1,4 +0,0 @@
-package br.com.alura.FindCar.model;
-
-public record Dados(String codigo, String nome) {
-}
--- Alura-Java Utilizando Lambdas,Streams,Spring FrameWorks/DesafiosPraticos/FindCar/src/main/java/br/com/alura/FindCar/model/modelos.java
+++
@@ -1,8 +0,0 @@
-package br.com.alura.FindCar.model;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.List;
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record modelos(List<Dados> modelos) {
-}
